7a236823185d92583fae1706275639dd87953305
Nathan Szalwinski <nathanszalwinski@gmail.com>
2014-01-19 12:18:36 -0600

    reverse done

---
+++ Text/reverse.py
@@ -0,0 +1,9 @@
+__author__ = 'Nathan'
+
+"""
+Reverse a String - Enter a string and the program
+will reverse it and print it out.
+"""
+
+string = raw_input("Enter a string, and I'll reverse it. ")
+print "%s reversed is %s" % (string, string[::-1])
